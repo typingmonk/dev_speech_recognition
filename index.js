db553b6c18766b93dda3b3c136703ba6d7863882
typingmonk <tmonk.hsieh@tmonk.tw>
2024-06-18 20:39:54 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+
+var recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.lang = 'zh-TW';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var button = document.getElementById('button');
+button.onclick = function() {
+  recognition.start();
+  console.log('Listening voice.');
+}
+
+
+recognition.onresult = function(event) {
+  //var transcript = event.results[0][0].transcript;
+  var transcript = event.results;
+  console.log(transcript);
+}
+
+recognition.onend = () => {
+  console.log('Speech recognition service disconnected');
+  restartRecognition(); // Restart the recognition service
+};
+
+recognition.onerror = (event) => {
+  console.error('Speech recognition error detected:', event.error);
+  restartRecognition(); // Restart on error
+};
+
+const restartRecognition = () => {
+  setTimeout(() => {
+    recognition.stop();
+    recognition.start();
+  }, 1000); // Restart after a short delay
+};
